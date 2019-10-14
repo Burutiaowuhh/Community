b9e3b9cc95b0cf17cbafe60619290335d7627db4
Burutiaowuhh <1409168426@qq.com>
2019-10-14 15:26:48 +0800

    bootstrap navigtion

--- src/main/java/com/mao/community/controller/HelloController.java
+++ src/main/java/com/mao/community/controller/HelloController.java
@@ -10,8 +10,12 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name") String name, Model model){
-
         model.addAttribute("name",name);
         return "hello";
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
